23dabba444fdbb2fdc2095ee718591325f6cf94a
Thang <phidinhthang@gmail.com>
2021-09-30 16:31:41 +0700

    continue fix bug...

--- server/src/utils/auth/sendRefreshToken.ts
+++ server/src/utils/auth/sendRefreshToken.ts
@@ -2,6 +2,7 @@ import { Response } from 'express';
 
 export const sendRefreshToken = (res: Response, refreshToken: string) => {
   return res.cookie('jid', refreshToken, {
-    httpOnly: true,
+    httpOnly: false,
+    domain: process.env.FRONTEND_NEXTJS_URL,
   });
 };
